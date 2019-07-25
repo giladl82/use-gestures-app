85734473ccc0e390abf9a22889332621b5c0fb15
Gilad Lev-Ari <giladl82@gmail.com>
2019-07-25 10:07:35 +0300

    Updates

---
+++ precache-manifest.3c30165ee8efad7757a4736d535bf5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d823a9f3d7606ff26c3bd102ea9fbcc1",
+    "url": "/use-gestures-app/index.html"
+  },
+  {
+    "revision": "6886b5e744fc645c8ed2",
+    "url": "/use-gestures-app/static/css/main.405b1edc.chunk.css"
+  },
+  {
+    "revision": "80dabe8e057946efa0a9",
+    "url": "/use-gestures-app/static/js/2.3b587ebb.chunk.js"
+  },
+  {
+    "revision": "6886b5e744fc645c8ed2",
+    "url": "/use-gestures-app/static/js/main.f18ae69b.chunk.js"
+  },
+  {
+    "revision": "ff4b11d415df37489328",
+    "url": "/use-gestures-app/static/js/runtime~main.3d00b3b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/use-gestures-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
